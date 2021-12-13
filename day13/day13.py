87cb21b5d3de296a49d3b8a37c1090b99c0dc891
Mateus Barros <mateus.rodrigues@geekie.com.br>
2021-12-13 12:30:28 -0300

    day 13 done

---
+++ day13/day13.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python3
+import sys
+
+input_file = sys.argv[1] if len(sys.argv) > 1 else 'input.txt'
+SIZE = 1500
+paper = [[0 for _ in range(1500)] for _ in range(1500)]
+instructions = []
+
+with open(input_file, 'r') as file:
+    for line in file:
+        if line == '\n':
+            break
+
+        y, x = line.strip().split(',')
+
+        paper[int(x)][int(y)] = 1
+
+    for line in file:
+        instruction = line.split('fold along ')[1].split('=')
+        instructions.append((instruction[0], int(instruction[1])))
+
+
+ANSWER_1 = 0
+
+
+def count_points():
+    global ANSWER_1
+    for i in range(SIZE):
+        for j in range(SIZE):
+            if paper[i][j] != 0:
+                ANSWER_1 += 1
+
+
+def fold(instructions):
+    for count, instruction in enumerate(instructions):
+        direction, pos = instruction
+
+        if direction == 'y':
+            for i in range(pos+1, SIZE):
+                for j in range(SIZE):
+                    paper[i-2*(i-pos)][j] += paper[i][j]
+                    paper[i][j] = 0
+        else:
+            for i in range(SIZE):
+                for j in range(pos+1, SIZE):
+                    paper[i][j-2*(j-pos)] += paper[i][j]
+                    paper[i][j] = 0
+
+        if count == 0:
+            count_points()
+
+
+def answer1():
+    fold(instructions)
+    print(ANSWER_1)
+
+
+def answer2():
+    for i in range(SIZE):
+        for j in range(SIZE):
+            if paper[i][j] != 0:
+                paper[i][j] = '#'
+            else:
+                paper[i][j] = ' '
+
+    for i in range(7):
+        line = ''
+        for count, c in enumerate(paper[i]):
+            if count < 50:
+                line += c
+
+        print(line)
+
+
+answer1()
+answer2()
